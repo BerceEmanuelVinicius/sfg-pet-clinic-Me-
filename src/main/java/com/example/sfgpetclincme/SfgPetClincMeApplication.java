224b876de3f494454874f9eab90fb6daad870e0c
berceemanuel-vinicius <berce.emanuel.vinicius@gmail.com>
2023-02-15 21:07:52 +0200

    Initial Project Add

---
+++ src/main/java/com/example/sfgpetclincme/SfgPetClincMeApplication.java
@@ -0,0 +1,13 @@
+package com.example.sfgpetclincme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClincMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgPetClincMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sfgpetclincme/SfgPetClincMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sfgpetclincme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClincMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

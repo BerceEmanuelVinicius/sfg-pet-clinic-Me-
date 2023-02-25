2b73bc61bfcf3a70388cb0f363570ff8b2b90fac
berceemanuel-vinicius <berce.emanuel.vinicius@gmail.com>
2023-02-25 11:38:05 +0200

    Updated base entity with JPA annotation. Closes #35

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -5,7 +5,6 @@ import java.time.LocalDate;
 
 public class Pet extends BaseEntity{
     private String name;
-
     private PetType petType;
     private Owner owner;
     private LocalDate birthDate;
